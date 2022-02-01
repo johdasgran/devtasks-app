c9a54b63557b3bd6955290378d859dae9333091b
Johdasgran <dampig14@gmail.com>
2022-02-01 09:13:39 -0500

    intro to React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import ToDoCounter from "./Components/ToDoCounter";
+
+
+const task = [
+  { text: "Curso intro react", completed: false},
+  { text: "Practicar ingles", completed: false},
+  { text: "Practicar UX/UI", completed: false}
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ToDoCounter /> 
+        <h1>Hahaha</h1>
+      
+
+    </React.Fragment>
   );
 }
 
---
+++ src/Components/ToDoCounter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ToDoCounter() {
+    return (
+        <React.Fragment>
+        <h2>Has completado 1 de 3 tareas.</h2>
+        </React.Fragment>
+    );
+}
+
+export default ToDoCounter;
