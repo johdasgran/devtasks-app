e742834004b17168bbd180aefdef440eff5414bc
Johdasgran <dampig14@gmail.com>
2022-02-06 16:38:07 -0500

    init design

--- src/Components/TaskCounter/TaskCounter.jsx
+++ src/Components/TaskCounter/TaskCounter.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function ToDoCounter({completed, inProgress}) {
     return (
+        <>
+        <h1>hello</h1>
         <h2>Has completado {completed} de {inProgress} tareas.</h2>
+        </>
     );
 }
 
