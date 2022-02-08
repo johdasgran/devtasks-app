8f4d42e6e236603882e81a2ac96adae9de944999
Johdasgran <dampig14@gmail.com>
2022-02-08 08:38:53 -0500

    update style task item

--- src/Components/TaskItem/TaskItem.jsx
+++ src/Components/TaskItem/TaskItem.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import "./TaskItem.css";
 
 function TaskItem(props) {
-
   console.log(props.completed);
 
-  let className = 'icon check';
+  let className = "icon check";
+  let classNameText = "note";
+
   if (props.completed) {
-    className = 'icon completed'
-  } 
+    className = "icon completed";
+    classNameText += " check-text";
+  }
+
 
 
   return (
     <li className="task-item">
       <span className={className} onClick={props.onComplete}></span>
-      <p className="note">{props.text}</p>
+      <p className={classNameText}>{props.text}</p>
       <span className="icon star"></span>
       <span className="icon trash" onClick={props.onDelete}></span>
     </li>
