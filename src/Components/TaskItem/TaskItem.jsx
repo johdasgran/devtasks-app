a9fff365bd767f025bcb34aecadd0b56b477b2a6
Johdasgran <dampig14@gmail.com>
2022-02-08 08:31:25 -0500

    update style task item

--- src/Components/TaskItem/TaskItem.jsx
+++ src/Components/TaskItem/TaskItem.jsx
@@ -2,23 +2,14 @@ import React from "react";
 import "./TaskItem.css";
 
 function TaskItem(props) {
-  // const onComplete = () => {
-  //   alert(`Has completado la tarea: ${props.text}`);
-  // };
-
-  // const onDelete = () => {
-  //   alert(`Has borrado la tarea: ${props.text}`);
-  // };
-
-  // { completed, inProgress }
 
   console.log(props.completed);
 
-  
   let className = 'icon check';
   if (props.completed) {
-    className = 'icon completed';
-  }
+    className = 'icon completed'
+  } 
+
 
   return (
     <li className="task-item">
